0e8b09fe7230ddb6f8869e30c92c481658081c28
Nicolas Cordin <dindonus@users.noreply.github.com>
2017-09-12 14:00:14 +0200

    restore main component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,21 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router';
+import { Link } from 'react-router-dom';
 
-const Main = React.createClass({
-
-  render() {
-    // Then we go ahead and return some JSX
-    return (
-      <div>
-        <h1>
-          <Link to="/">Reduxstagram</Link>
-        </h1>
-        {/* We use cloneElement here so we can auto pass down props */}
-        { React.cloneElement(this.props.children, this.props) }
-      </div>
-    );
-  }
-
-});
+const Main = ({ children }) => (
+  <div>
+    <h1>
+      <Link to="/">Reduxstagram</Link>
+    </h1>
+    { children }
+  </div>
+)
 
 export default Main;
--- src/index.js
+++ src/index.js
@@ -1,35 +1,24 @@
-/*
-  Import Dependencies
-*/
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
-import 'babel-polyfill';
 
-/*
-  Import Components
-*/
 import Single from './containers/Single';
 import PhotoGrid from './containers/PhotoGrid';
+import Main from './components/Main';
 
-/* Import CSS */
 import css from  './styles/style.styl';
-
-/* Import our data store */
 import store from './store';
 
-/*
-  Rendering
-  This is where we hook up the Store with our actual component and the router
-*/
 render(
   <Provider store={store}>
     <BrowserRouter>
+      <Main>
       <Switch>
         <Route exact path="/" component={PhotoGrid} />
         <Route path="/view/:postId" component={Single} />
       </Switch>
+    </Main>
     </BrowserRouter>
   </Provider>,
   document.getElementById('root')
